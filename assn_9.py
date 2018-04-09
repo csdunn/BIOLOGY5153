c062fe53bddc33903ca5100002c9b9ae473eb76c
Celeste Dunn <csdunn@uark.edu>
2018-04-09 14:37:44 -0500

    updated code

--- assn_9.py
+++ assn_9.py
@@ -1,28 +1,21 @@
 #!/
 
-#read in the genome(FASTA format) and store it in a variable-use SeqIO for this
-
 #open watermelon.gff
-watermelongff = open("/Users/Celeste/Desktop/watermelon_files/watermelon.gff")
+watermelon = open("/Users/Celeste/Desktop/watermelon_files/watermelon.gff")
+
+#open watermelon.fsa
+watermelonfsa = open("/Users/Celeste/Desktop/watermelon_files/watermelon.fsa").read()
 
 #read gff in line by line using a for loop
 #split line into list,grab 4th and 5th on list, print it out to the screen, and continue to the next line, print 
-for line in watermelongff:
-	positions = line.split("\t")   
-	start = positions[3]
-	stop = positions[4]
-	print("start codon is " + start + " and stop codon is " + stop)
-	print("start codon is " + start + " and stop codon is " + stop)
-	trimmedDNA = [start + ":"  +  stop]
-	print(trimmedDNA)
-
-#close gff file
-watermelongff.close()
-
-#open watermelon.fsa
-watermelonfsa = open("/Users/Celeste/Desktop/watermelon_files/watermelon.fsa")
-
-#use start and stop codon to pull sequence from watermelon.fsa
-
-#print sequence with start and stop codon
+for line in watermelon:
+    positions = line.split("\t")   
+    start = (positions[3])
+    stop = (positions[4])
+    print("start codon is " + start + " and stop codon is " + stop)
+#change positions to integers, pull sequence from watermelon.fsa, print    
+    start_2 = int(positions[3])
+    stop_2 = int(positions[4])
+    trimmedDNA_2 = watermelonfsa[start_2:stop_2]
+    print(trimmedDNA_2)
 
